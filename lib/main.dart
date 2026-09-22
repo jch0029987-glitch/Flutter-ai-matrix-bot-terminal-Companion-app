0830019d450884eafd20bf52f0095a4ff32faffc
jch0029987-glitch <jch0029987@gmail.com>
2026-09-21 20:07:24 -0500

    Change mainAxisAlignment to spaceBetween in Column

--- lib/main.dart
+++ lib/main.dart
@@ -466,7 +466,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Row(
-                                  mainAxisAlignment: MainAxisAlignment.between,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                   children: [
                                     Text(
                                       _isFullScreenTerminal 
